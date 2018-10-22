c0ddeeaffb67c3f0c38cdcfceb0a0cf324e78f7c
gopiprasanth potipireddy <gopi.prasanth007@gmail.com>
2018-10-22 23:54:25 +0530

    Added photo album app
    Node js api to photo album

---
+++ content/home.js
@@ -0,0 +1,24 @@
+$( function (){
+var tmpl,
+// Main template HTML
+tdata = {}; // JSON data object that feeds the template
+// Initialize page
+var initPage = function () {// Load the HTML template
+$.get("/templates/home.html", function(d){
+tmpl = d;
+});
+// Retrieve the server data and then initialize the page
+$.getJSON("/albums.json", function (d) {
+$.extend(tdata, d.data);
+});
+// When AJAX calls are complete parse the template
+// replacing mustache tags with vars
+$(document).ajaxStop(function () {
+var renderedPage = Mustache.to_html(tmpl, tdata);
+$("body").html(renderedPage);
+})
+}();
+// 1
+// 2
+// 3
+});
